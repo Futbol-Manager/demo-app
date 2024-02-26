e1e0631f2e9b813cb9be4b87807d34619d9b4989
jaime-dela <jdelaossaf@gmail.com>
2024-02-26 07:52:46 -0500

    creacion inicio y login
    esto solo son las pantallas como tal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'login', component: LoginComponent },
+  // Puedes agregar más rutas según sea necesario
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
