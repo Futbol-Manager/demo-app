f3f64c857a8f0cf17af1d1184e0255f444a34d4f
salware-education <alejandro.arevalo@salware.com>
2024-03-05 11:30:08 +0100

    login tras el registro

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NbToastrService } from '@nebular/theme';
+import { LoginModel } from 'src/app/core/models/users/login.model';
 import { GenreTypeModel, ProfileTypeModel, RegisterModel } from 'src/app/core/models/users/register.model';
+import { LoginService } from 'src/app/core/services/login/login.service';
 import { RegisterService } from 'src/app/core/services/register/register.service';
 
 @Component({
@@ -18,6 +20,7 @@ export class RegisterComponent implements OnInit {
     private router: Router,
     private registerService: RegisterService,
     private toastrService: NbToastrService,
+    private loginService: LoginService,
   ) {
     this.registerForm = new FormGroup({
       rol: new FormControl('', [Validators.required]),
@@ -64,12 +67,30 @@ export class RegisterComponent implements OnInit {
         (res) => {
           if(res.data != null) {
             this.toastrService.show('Inicia sesión para iniciar', 'Registro Exitoso', { status: 'success' });
-            this.router.navigate(['/home'])
+            // Llamando al método login después de un registro exitoso
+            this.login();
           } else{
             this.toastrService.show('Intentalo de nuevo', 'Error en el registro', { status: 'error' });
           }
         })
     }
+  }
+
+  login() {
+    // Aquí, llama al método login y realiza la redirección después de un inicio de sesión exitoso
+    const fv = this.registerForm.value;
+    const login: LoginModel = new LoginModel(
+      (fv.email as string).trim(),
+      (fv.password as string).trim(),
+    );
+    
+    this.loginService.login(login).pipe()
+      .subscribe(
+        (res) => {
+          if (res.data != null) {
+            this.router.navigate(['/inicio']);
+          }
+        });
   }
 
 }
