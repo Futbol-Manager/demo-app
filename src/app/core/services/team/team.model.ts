a52bfc0b2084b2a2b494a39558e89bcccbda60ad
salware-education <alejandro.arevalo@salware.com>
2024-03-29 00:33:32 +0100

    add jugadores y estilos inicio

--- src/app/core/services/team/team.model.ts
+++ src/app/core/services/team/team.model.ts
@@ -1,20 +1,38 @@
 // team.model.ts
 
 export class Team {
-    teamId: number;
-    levelLeague: string;
-    name: string;
-    trainingDays: string;
-    category: string;
+  teamId: number;
+  levelLeague: string;
+  name: string;
+  trainingDays: string;
+  category: string;
 
-    constructor(object: any) {
-      this.teamId = (object.teamId) ? object.teamId : null;
-      this.levelLeague = (object.levelLeague) ? object.levelLeague : null;
-      this.name = (object.name) ? object.name : null;
-      this.trainingDays = (object.trainingDays) ? object.trainingDays : null;
-      this.category = (object.category) ? object.category : null;
-    }
+  constructor(object: any) {
+    this.teamId = (object.teamId) ? object.teamId : null;
+    this.levelLeague = (object.levelLeague) ? object.levelLeague : null;
+    this.name = (object.name) ? object.name : null;
+    this.trainingDays = (object.trainingDays) ? object.trainingDays : null;
+    this.category = (object.category) ? object.category : null;
   }
+}
+
+export class TeamConJugadores {
+  teamId: number;
+  levelLeague: string;
+  name: string;
+  trainingDays: string;
+  category: string;
+  jugadoresPorEquipo: string;
+
+  constructor(object: any) {
+    this.teamId = (object.teamId) ? object.teamId : null;
+    this.levelLeague = (object.levelLeague) ? object.levelLeague : null;
+    this.name = (object.name) ? object.name : null;
+    this.trainingDays = (object.trainingDays) ? object.trainingDays : null;
+    this.category = (object.category) ? object.category : null;
+    this.jugadoresPorEquipo = (object.jugadoresPorEquipo) ? object.jugadoresPorEquipo : null;
+  }
+}
 
 // En el modelo Team
 export class TeamNew {
