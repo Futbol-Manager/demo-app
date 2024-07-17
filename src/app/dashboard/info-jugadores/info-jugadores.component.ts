b61dacace41af4f50903da1f7209f73c083f1520
salware-education <alejandro.arevalo@salware.com>
2024-07-17 12:35:15 +0200

    update tabla

--- src/app/dashboard/info-jugadores/info-jugadores.component.ts
+++ src/app/dashboard/info-jugadores/info-jugadores.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -13,13 +13,17 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './info-jugadores.component.html',
   styleUrls: ['./info-jugadores.component.scss']
 })
-export class InfoJugadoresComponent implements OnInit {
+export class InfoJugadoresComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  @ViewChild('dataTable', { static: false })
+  table!: ElementRef;
 
   clubId = 0;
   datosCargados = false;
   teams: any[] = [];
   players: any[] = [];
   teamSelected: number = -1;
+  dataTable: any; // Variable para almacenar la referencia a la tabla DataTable
 
   constructor(
     private router: Router,
@@ -40,6 +44,16 @@ export class InfoJugadoresComponent implements OnInit {
     this.cargarListadoJugadores();
   }
 
+  ngAfterViewInit(): void {
+    this.inicializarDataTable();
+  }
+
+  ngOnDestroy(): void {
+    if (this.dataTable) {
+      this.dataTable.destroy();
+    }
+  }
+
   // Método para redirigir a la pantalla de jugadores con el teamId
   irAPantalla(id: number): void {
     switch (id) {
@@ -56,13 +70,11 @@ export class InfoJugadoresComponent implements OnInit {
         if (response && response.data) {
           // Mapea los datos bajo 'data' a instancias del modelo Team
           this.teams = response.data.teams; 
-          /*for (let i = 0; i < this.teams.length; i++) {
+          for (let i = 0; i < this.teams.length; i++) {
             for (let a = 0; a < this.teams[i].players.length; a++) {
               this.players.push(this.teams[i].players[a]);              
             }            
-          }*/
-          this.inicializarDataTable();
-          this.datosCargados = true;
+          }
         } else {
           console.error('La respuesta del servicio no tiene la estructura esperada', response);
         }
@@ -72,34 +84,24 @@ export class InfoJugadoresComponent implements OnInit {
         console.error('Error al cargar el listado de jugadores', error);
       }
     );
-  }// Método para inicializar el DataTable
+  }
+  
+  // Método para inicializar el DataTable
   inicializarDataTable(): void {
-    // Destruir el DataTable si ya existe
-    const $dataTable = $('#dataTable');
-    if ($dataTable.hasClass('dataTable')) {
-      $dataTable.DataTable().destroy();
-    }
-
-    this.http.get('assets/dataTable/Spanish.json').subscribe((translation) => {
-      $(document).ready(function () {
-        $('#dataTable').DataTable({
-          paging: true,
-          pageLength: 100,
-          searching: true,
-          ordering: true,
-          order: [[0, 'desc']],
-          columnDefs: [
-            {
-              targets: [0],
-              visible: false
-            }
-          ],
-          language: translation
-        });
-      });
+    const table: any = $(this.table.nativeElement);
+    this.dataTable = table.DataTable({
+      paging: true,
+      pageLength: 100,
+      searching: true,
+      ordering: true,
+      order: [[0, 'asc']], // Ordenar por la primera columna
+      language: {
+        url: 'assets/dataTable/Spanish.json' // URL del archivo de traducción
+      }
     });
 
     this.moverElementosDataTable();
+    this.datosCargados = true;
   }
 
 
@@ -166,14 +168,20 @@ export class InfoJugadoresComponent implements OnInit {
     search.observe(this.elementRef.nativeElement, { childList: true, subtree: true });
   }
 
-  loadPlayersOfTeam(){
-    if(this.teamSelected < 0) {
-      this.players = [];
-      for (let b = 0; b < this.teams.length; b++) {
-        this.players.push(this.teams[b].players);
-      }
+  loadPlayersOfTeam(): void {
+    if (this.teamSelected < 0) {
+      this.players = this.teams.flatMap(team => team.players); // Mostrar todos los jugadores
     } else {
-      this.players.push(this.teams[this.teamSelected].players);
+      this.players = this.teams[this.teamSelected].players; // Mostrar jugadores del equipo seleccionado
+    }
+    this.actualizarTabla();
+  }
+
+  actualizarTabla(): void {
+    if (this.dataTable) {
+      this.dataTable.clear();
+      this.dataTable.rows.add(this.players);
+      this.dataTable.draw();
     }
   }
 
